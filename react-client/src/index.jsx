92a2de0df3651353b198e2d7e7b90291b8963014
khengschew <khengc@gmail.com>
2019-03-17 16:00:01 -0700

    Implement websocket connection

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
 import Board from './components/Board';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      socket: io(),
+    };
   }
 
   render () {
--- server/index.js
+++ server/index.js
@@ -1,14 +1,28 @@
 var express = require('express');
+var http = require('http');
+var socketio = require('socket.io');
 var bodyParser = require('body-parser');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
 // var items = require('../database-mongo');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketio(server);
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+io.on('connection', client => {
+  console.log('client connected');
+  client.on('event', data => {
+    console.log(data);
+  });
+
+  client.on('disconnect', () => {
+    console.log('disconnecting client');
+  });
 });
 
+server.listen(3000, function() {
+  console.log('listening on port 3000!');
+});
